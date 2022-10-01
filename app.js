247b64c5b29a41a660f3c13c7042593270d1b415
JanHryb <jan.hryb@gmail.com>
2022-10-01 22:38:11 +0200

    made basic structure of project

---
+++ app.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+//routing
+app.use("/", require("./routes/home"));
+
+//TODO:app.get("*", (req, res) => {});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
